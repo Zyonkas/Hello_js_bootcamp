3abe9300afed3db51a5fecdb3cc3289fa6127a1f
Zyonkas <zhujiankai25@gmail.com>
2022-10-05 17:50:32 +0200

    tablas lista

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
-function cuentaClicks(event) {
-    alert("click")
+function addRow(event) {
+    let texto = document.querySelector("#entrada").value
+    let tabla = document.querySelector("#la_tabla")
+    // TODO: Crear una fila a la tabla la_tabla debajo del boton con el texto de input
+    const fila = document.createElement("tr")
+    const celda = document.createElement("td")
+    celda.innerHTML = texto
+    
+    fila.appendChild(celda)
+    tabla.appendChild(fila)
+    
 }
 
 window.onload = function() {
     let elboton = document.querySelector("#crear")
-    elboton.addEventListener("click", cuentaClicks)
+    elboton.addEventListener("click", addRow)
+
 }
+
